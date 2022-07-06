5901857e4b5eae39d7a5bbd35b18f2810785fbee
las lilian <las.lian@icloud.com>
2022-07-05 22:46:23 -0300

    ✔️Atualizado

--- main.js
+++ main.js
@@ -25,45 +25,45 @@ window.addEventListener('scroll', () => {
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
-    content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
+    
     image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
 });
 
-// Navbar
+//Navbar
 const navSlide = () => {
-    const burguer = document.querySelector('.burguer');
-    const nav = document.querySelector('.nav-list');
-    const navList = document.querySelectorAll('.nav-list li');
+const burguer = document.querySelector('.burguer');
+const nav = document.querySelector('.nav-list');
+const navList = document.querySelectorAll('.nav-list li');
 
-    // Toggle nav
-    burguer.addEventListener('click',() => {
-        nav.classList.toggle('nav-active');
+//Toggle nav
+burguer.addEventListener('click',() => {
+nav.classList.toggle('nav-active');
 
-        // Animate list
-        navList.forEach((list, index) => {
-            if(list.style.animation){
-                list.style.animation = '';
-            } else {
-                list.style.animation = `navListFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
+//Animate list
+navList.forEach((list, index) => {
+if(list.style.animation){
+ list.style.animation = '';
+} else {
+    list.style.animation = `navListFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+}
+});
 
-        // Burguer animation
-        burguer.classList.toggle('toggle');
-        });
+//Burguer animation
+burguer.classList.toggle('toggle');
+});
 }
 
 navSlide();
 
-// slider automatic
+//slider automatic
 
 var counter = 1;
 setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
+ document.getElementById('radio' + counter).checked = true;
+counter++;
     if (counter > 4) {
         counter = 1;
-    }
+}
 }, 5000);
